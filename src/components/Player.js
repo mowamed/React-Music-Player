8adaf053534ddf58fd8968da915a4be6dec8ac1a
Mohamed Bakayoko <mowamedbakus@gmail.com>
2021-01-04 22:49:21 +0000

    add play pause behavior

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,7 +1,21 @@
+import { useRef } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
-function Player() {
+function Player({ currentSong, isPlaying, setIsPlaying }) {
+
+    const audioRef = useRef(null);
+
+    const playSongHandler = () => {
+        if (isPlaying) {
+            audioRef.current.pause();
+            setIsPlaying(!isPlaying);
+        } else {
+            audioRef.current.play();
+            setIsPlaying(!isPlaying);
+        }
+
+    }
     return (
         <div className="player">
             <div className="time-control">
@@ -9,10 +23,11 @@ function Player() {
                 <input type="range" />
                 <p>End Time</p>
             </div>
-            <div class="play-control">
+            <div className="play-control">
                 <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
-                <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+                <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay} />
                 <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
+                <audio ref={audioRef} src={currentSong.audio}></audio>
             </div>
         </div>
     );
